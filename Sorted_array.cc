158bb4977a6b057af7515bfa6ce95b9e78cf8565
chadtied <96424234+chadtied@users.noreply.github.com>
2022-12-17 07:18:39 +0800

    Create Sorted_array.cc

---
+++ Sorted_array.cc
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<ctime>
+#include<cmath>
+
+using namespace std;
+
+//if a data is input , use the concept of insertion sort to insert it to the correct order
+void insert(vector<int>& arr, int input){
+        arr.push_back(input);
+        for(int i= arr.size()-2; i>= 0; i--){
+                if(arr[i]<= input){
+                        break;
+                }
+                else{
+                        arr[i]= arr[i]^arr[i+1];
+                        arr[i+1]= arr[i]^arr[i+1];
+                        arr[i]= arr[i]^arr[i+1];
+                }
+        }
+}
+//use binary_search to find the data
+void search(vector<int>& arr, int head, int tail, int input){
+        int mid= (head+tail)/2;
+        if(arr[mid]== input){
+                return;
+        }
+        else if(mid== head){
+                return;
+        }
+        else if(input> arr[mid]){
+                search(arr, mid+1, tail, input);
+        }
+        else{
+                search(arr, head, mid-1, input);
+        }
+}
+int main(){
+        srand(time(NULL));
+        vector<int> arr;
+        double start, end;
+        int num;
+        cin>> num;
+        num= pow(2,num);
+
+        start= clock();
+        for(int i= 0; i< num; i++){
+                insert(arr, rand()%100000000);
+        }
+        end= clock();
+        cout<< "insertion costs "<< (end- start)/1000<< "sec"<< endl;
+
+        start= clock();
+        for(int i= 0; i< 100000; i++){
+                search(arr, 0, arr.size(), rand()%100000000);
+        }
+
+        end= clock();
+        cout<< "traverse costs "<< (end- start)/1000<< "sec"<< endl;
+}
